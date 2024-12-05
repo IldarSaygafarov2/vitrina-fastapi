5e11f2e6fa9475ef94e0ce4e774c13ea4c80e759
IldarSaygafarov2 <sayildar17@gmail.com>
2024-12-05 19:08:57 +0500

    added corses

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.api import router as api_router
 from backend.app.config import config
-from fastapi.staticfiles import StaticFiles
+
 main_app = FastAPI()
 main_app.include_router(api_router)
 
 main_app.mount("/media", StaticFiles(directory="media"), name="media")
+
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(
