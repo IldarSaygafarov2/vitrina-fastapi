88363826e931d1ad516c77f6c6ceef6fc0d66b3c
IldarSaygafarov2 <sayildar17@gmail.com>
2026-04-13 23:56:01 +0500

    fixes

--- scripts/fill_data/agent/fill_missing_col.py
+++ scripts/fill_data/agent/fill_missing_col.py
@@ -60,7 +60,9 @@ async def fill_missing_col(session: AsyncSession):
         headers[-1] = headers[-1].capitalize()
         records = sheet.get_all_records(expected_headers=headers)
 
-        sheet.delete_rows(2, len(records))
+        if records:
+            sheet.delete_rows(2, len(records))
+
         sheet.insert_rows(agent_advertisements, row=2)
         await asyncio.sleep(2)
 
